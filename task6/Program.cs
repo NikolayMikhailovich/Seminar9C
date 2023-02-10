9e2f5978c73fd3b001674a7fc88fa7e8693871cc
nikolays <sidorovkolya91@gmail.com>
2023-02-10 22:51:00 +0300

    Проверка на простое число:

---
+++ task6/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Проверка на простое число: 
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+int Promt(string msg)
+{
+  System.Console.Write(msg + "-> ");
+  return int.Parse(Console.ReadLine());
+}
+
+bool CheskPrimary(int number, int divider)
+{
+  if (divider == 1)
+    return true;
+  if (number % divider == 0)
+    return false;
+  return CheskPrimary(number, divider - 1);
+}
+
+int number = Promt("Введите число ");
+bool result = CheskPrimary(number, number - 1);
+System.Console.WriteLine(result);
