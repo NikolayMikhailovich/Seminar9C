cdf072b634e109c3e24be29fd58dab6605f77d82
nikolays <sidorovkolya91@gmail.com>
2023-02-10 22:48:05 +0300

    Напишите программу, которая будет принимать на вход число и // возвращать сумму его цифр. Использовать рекурсию.

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и 
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int Promt(string msg)
+{
+  System.Console.Write(msg + "-> ");
+  return int.Parse(Console.ReadLine());
+}
+
+int ShowNumbers(int num)
+{
+  if (num == 0)
+  {
+    return 0;
+  }
+  return num % 10 + ShowNumbers(num / 10);
+}
+
+int number = Promt("Введите число ");
+int result = ShowNumbers(number);
+System.Console.WriteLine(result);
